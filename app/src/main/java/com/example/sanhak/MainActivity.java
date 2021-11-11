a2701b045735e1912642d4cc901e4aadbe66ba4c
SongJH97 <83583546+SongJH97@users.noreply.github.com>
2021-11-12 03:28:08 +0900

    송주한 프론트

---
+++ app/src/main/java/com/example/sanhak/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.sanhak;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView; //하단바
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private MyList myList;
+    private MyPage myPage;
+    private MyMain myMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.main:
+                        setFrag(0);
+                        break;
+                    case R.id.my_list:
+                        setFrag(1);
+                        break;
+                    case R.id.my_page:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+        myMain = new MyMain();
+        myList = new MyList();
+        myPage = new MyPage();
+        setFrag(0);
+
+
+    }
+
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n){
+            case 0:
+                ft.replace(R.id.main_frame, myMain);
+                ft.commit();
+                break;
+
+            case 1:
+                ft.replace(R.id.main_frame, myList);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, myPage);
+                ft.commit();
+                break;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/sanhak/MyPage.java
@@ -0,0 +1,26 @@
+package com.example.sanhak;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyPage extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_my_page,container, false);
+
+        return view;
+    }
+}
