efe0d21e49dabdd3a60395cf50faf93730886141
yeongjin <hanyj0104@naver.com>
2021-12-01 01:47:05 +0900

    jin

--- app/src/main/java/com/example/sanhak/Item.java
+++ app/src/main/java/com/example/sanhak/Item.java
@@ -1,2 +1,67 @@
-package com.example.sanhak;public class Item {
+package com.example.sanhak;
+
+public class Item {
+    private String id;
+    private String name;
+    private String img;
+    private String color;
+    private String type;
+    private String url;
+
+    public Item(String id, String name, String img, String color, String type, String url) {
+        this.id = id;
+        this.name = name;
+        this.img = img;
+        this.color = color;
+        this.type = type;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- app/src/main/java/com/example/sanhak/ItemAdapter.java
+++ app/src/main/java/com/example/sanhak/ItemAdapter.java
@@ -1,2 +1,68 @@
-package com.example.sanhak;public class ItemAdapter {
-}
+//package com.example.sanhak;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.bumptech.glide.Glide;
+//import com.google.android.ads.mediationtestsuite.viewmodels.ItemViewHolder;
+//import com.google.firebase.database.core.Context;
+//
+//import java.util.ArrayList;
+//
+//public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.itemViewHolder>{
+//private ArrayList<Item> arrayList;
+//private Context context;
+//
+//    public ItemAdapter(ArrayList<Item> arrayList, Context context) {
+//        this.arrayList = arrayList;
+//        this.context = context;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public itemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+//       ItemViewHolder holder=new ItemViewHolder(view);
+//    return holder;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull itemViewHolder holder, int position) {
+//        Glide.with(holder.itemView)
+//                .load(arrayList.get(position).getProfile())
+//                .into(holder.iv_profile);//load image from server
+//        //  holder.iv_profile.setImageDrawable(arrayList.get(position).getProfile());
+//        holder.tv_id.setText(arrayList.get(position).getId());
+//        holder.tv_pw.setText(String.valueOf(arrayList.get(position).getPw()));
+//        holder.tv_userName.setText(arrayList.get(position).getUserName());
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//
+//    public class itemViewHolder extends RecyclerView.ViewHolder {
+//        ImageView iv_profile;
+//        TextView tv_id;
+//        TextView tv_pw;
+//        TextView tv_userName;
+//
+//        public itemViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            this.iv_profile=itemView.findViewById(R.id.iv_profile);
+//            this.tv_id=itemView.findViewById(R.id.tv_id);
+//            this.tv_pw=itemView.findViewById(R.id.tv_pw);
+//            this.tv_userName=itemView.findViewById(R.id.tv_userName);
+//        }
+//    }
+//}
+
+
